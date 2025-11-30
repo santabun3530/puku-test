7b4747688aef9d99b40ab5c28e812207e255584a
santabun <santabun3530@gmail.com>
2025-11-30 13:19:40 +0000

    add

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,106 +1,116 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8001';
-const RECIPE_SERVICE_URL = process.env.REACT_APP_RECIPE_SERVICE_URL || 'http://localhost:8002';
-const RATING_SERVICE_URL = process.env.REACT_APP_RATING_SERVICE_URL || 'http://localhost:8003';
+// All requests go through Ingress
+const API_BASE_URL = "/api/users";
+const RECIPE_BASE_URL = "/api/recipes";
+const RATING_BASE_URL = "/api/ratings";
+
+// User/Auth API -----------------------------
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
 
-
 export const authService = {
   login: async (username, password) => {
     const formData = new FormData();
-    formData.append('username', username);
-    formData.append('password', password);
-    
-    const response = await api.post('/token', formData);
+    formData.append("username", username);
+    formData.append("password", password);
+
+    const response = await api.post("/token", formData);
     return response.data;
   },
-  
+
   register: async (userData) => {
-    const response = await api.post('/register', userData);
+    const response = await api.post("/register", userData);
     return response.data;
   },
-  
+
   getCurrentUser: async () => {
-    const response = await api.get('/users/me');
+    const response = await api.get("/users/me");
     return response.data;
   }
 };
+
+
+// Recipe API --------------------------------
 
 export const recipeService = {
+
   getRecipes: async () => {
-    const response = await axios.get(`${RECIPE_SERVICE_URL}/recipes`);
-    return response.data;
+    const res = await axios.get(`${RECIPE_BASE_URL}`);
+    return res.data;
   },
-  
+
   getRecipe: async (id) => {
-    const response = await axios.get(`${RECIPE_SERVICE_URL}/recipes/${id}`);
-    return response.data;
+    const res = await axios.get(`${RECIPE_BASE_URL}/${id}`);
+    return res.data;
   },
-  
+
   createRecipe: async (recipeData) => {
-    const token = localStorage.getItem('token');
-    const response = await axios.post(`${RECIPE_SERVICE_URL}/recipes`, recipeData, {
+    const token = localStorage.getItem("token");
+    const res = await axios.post(`${RECIPE_BASE_URL}`, recipeData, {
       headers: { Authorization: `Bearer ${token}` }
     });
-    return response.data;
+    return res.data;
   },
-  
+
   updateRecipe: async (id, recipeData) => {
-    const token = localStorage.getItem('token');
-    const response = await axios.put(`${RECIPE_SERVICE_URL}/recipes/${id}`, recipeData, {
+    const token = localStorage.getItem("token");
+    const res = await axios.put(`${RECIPE_BASE_URL}/${id}`, recipeData, {
       headers: { Authorization: `Bearer ${token}` }
     });
-    return response.data;
+    return res.data;
   },
-  
+
   deleteRecipe: async (id) => {
-    const token = localStorage.getItem('token');
-    const response = await axios.delete(`${RECIPE_SERVICE_URL}/recipes/${id}`, {
+    const token = localStorage.getItem("token");
+    const res = await axios.delete(`${RECIPE_BASE_URL}/${id}`, {
       headers: { Authorization: `Bearer ${token}` }
     });
-    return response.data;
+    return res.data;
   }
 };
 
+
+// Rating API --------------------------------
+
 export const ratingService = {
+
   getRecipeRatings: async (recipeId) => {
-    const response = await axios.get(`${RATING_SERVICE_URL}/recipes/${recipeId}/ratings`);
-    return response.data;
+    const res = await axios.get(`${RATING_BASE_URL}/recipes/${recipeId}/ratings`);
+    return res.data;
   },
-  
+
   createRating: async (ratingData) => {
-    const token = localStorage.getItem('token');
-    const response = await axios.post(`${RATING_SERVICE_URL}/ratings`, ratingData, {
+    const token = localStorage.getItem("token");
+    const res = await axios.post(`${RATING_BASE_URL}/ratings`, ratingData, {
       headers: { Authorization: `Bearer ${token}` }
     });
-    return response.data;
+    return res.data;
   },
-  
+
   updateRating: async (id, ratingData) => {
-    const token = localStorage.getItem('token');
-    const response = await axios.put(`${RATING_SERVICE_URL}/ratings/${id}`, ratingData, {
+    const token = localStorage.getItem("token");
+    const res = await axios.put(`${RATING_BASE_URL}/ratings/${id}`, ratingData, {
       headers: { Authorization: `Bearer ${token}` }
     });
-    return response.data;
+    return res.data;
   },
-  
+
   deleteRating: async (id) => {
-    const token = localStorage.getItem('token');
-    const response = await axios.delete(`${RATING_SERVICE_URL}/ratings/${id}`, {
+    const token = localStorage.getItem("token");
+    const res = await axios.delete(`${RATING_BASE_URL}/ratings/${id}`, {
       headers: { Authorization: `Bearer ${token}` }
     });
-    return response.data;
+    return res.data;
   }
 };
